3ce759ce5e100a0ffec145a6838f3fe6484f9faa
SAPPHIRE\aschnell <a.schnell@wtschnell.de>
2017-08-16 22:16:58 +0200

    ...

---
+++ src/FluiTec.AppFx.Data.Sql/Adapters/PostgreSqlAdapter.cs
@@ -0,0 +1,21 @@
+﻿using FluiTec.AppFx.Data.Sql.Mappers;
+
+namespace FluiTec.AppFx.Data.Sql.Adapters
+{
+	/// <summary>	A postgre SQL adapter. </summary>
+	public class PostgreSqlAdapter : SqlAdapter
+	{
+		/// <summary>	Default constructor. </summary>
+		public PostgreSqlAdapter()
+		{
+		}
+
+		/// <summary>	Constructor. </summary>
+		/// <param name="entityNameMapper">	The entity name mapper. </param>
+		public PostgreSqlAdapter(IEntityNameMapper entityNameMapper) : base(entityNameMapper)
+		{
+		}
+
+		public override string RenderTableName(string tableName) => $"\"{tableName}\"";
+	}
+}
