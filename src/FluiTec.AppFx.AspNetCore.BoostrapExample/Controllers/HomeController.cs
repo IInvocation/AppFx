32fc9198b17f6b0e9fab25875effb6f3bf4f1279
Achim Schnell <a.schnell@wtschnell.de>
2017-10-12 16:13:32 +0200

    Started adding simple Bootstrap-Example

---
+++ src/FluiTec.AppFx.AspNetCore.BoostrapExample/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluiTec.AppFx.AspNetCore.BoostrapExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
