b333cb96534b3c29fe1f105f89064600893ed6e4
Achim Schnell <a.schnell@wtschnell.de>
2017-12-14 12:00:20 +0100

    Added some extension-methods

---
+++ src/FluiTec.AppFx.AspNetCore/IdentityExtension.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using FluiTec.AppFx.AspNetCore.Configuration;
+using FluiTec.AppFx.Identity;
+using FluiTec.AppFx.Identity.Entities;
+using FluiTec.AppFx.Identity.Localization;
+using FluiTec.AppFx.Options;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluiTec.AppFx.AspNetCore
+{
+    /// <summary>	An identity server extension. </summary>
+	public static class IdentityExtension
+    {
+        /// <summary>	An IServiceCollection extension method that configure identity server. </summary>
+        /// <param name="services">			The services to act on. </param>
+        /// <param name="configuration">	The configuration. </param>
+        /// <returns>	An IServiceCollection. </returns>
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services,
+            IConfigurationRoot configuration)
+        {
+            var options = configuration.GetConfiguration<ApiOptions>();
+            services.AddSingleton(options);
+
+            // configure aspnet-identity
+            services.AddIdentity<IdentityUserEntity, IdentityRoleEntity>(config =>
+            {
+                config.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ öäüÖÄÜ ß 12345678";
+                config.SignIn.RequireConfirmedEmail = true;
+                config.Lockout.AllowedForNewUsers = true;
+                config.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            })
+                .AddErrorDescriber<MultiLanguageIdentityErrorDescriber>()
+                .AddDefaultTokenProviders();
+            services.AddIdentityStores();
+
+            // configure cookies
+            services.ConfigureApplicationCookie(cOptions =>
+            {
+                cOptions.Events = new CookieAuthenticationEvents
+                {
+                    // disable redirect to login for api-users
+                    OnRedirectToLogin = context =>
+                    {
+                        if (context.Request.Path.StartsWithSegments(options.ApiOnlyPath) &&
+                            context.Response.StatusCode == (int)HttpStatusCode.OK)
+                            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        else
+                            context.Response.Redirect(context.RedirectUri);
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            // configure aspnet-authentication
+            services.AddAuthentication()
+                .AddGoogle(gOptions =>
+                {
+                    gOptions.ClientId = configuration["Authentication:Google:ClientId"];
+                    gOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+                })
+                .AddAmazon(aOptions =>
+                {
+                    aOptions.ClientId = configuration["Authentication:Amazon:ClientId"];
+                    aOptions.ClientSecret = configuration["Authentication:Amazon:ClientSecret"];
+                });
+
+            return services;
+        }
+    }
+}
---
+++ src/FluiTec.AppFx.AspNetCore/MailExtension.cs
@@ -0,0 +1,38 @@
+﻿using FluiTec.AppFx.Mail;
+using FluiTec.AppFx.Mail.Configuration;
+using FluiTec.AppFx.Options;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RazorLight;
+
+namespace FluiTec.AppFx.AspNetCore
+{
+    /// <summary>	A mail service extension. </summary>
+    public static class MailServiceExtension
+    {
+        /// <summary>	Configure mail service. </summary>
+        /// <param name="services">			The services. </param>
+        /// <param name="environment">  	The environment. </param>
+        /// <param name="configuration">	The configuration. </param>
+        /// <returns>	An IServiceCollection. </returns>
+        public static IServiceCollection ConfigureMailService(this IServiceCollection services, IConfigurationRoot configuration, IHostingEnvironment environment)
+        {
+            services.AddSingleton(configuration.GetConfiguration<MailServiceOptions>());
+            services.AddRazorLightSelf(environment, "MailViews");
+            services.AddScoped<ITemplatingMailService, MailKitTemplatingMailService>();
+
+            return services;
+        }
+
+        /// <summary>   An IServiceCollection extension method that adds a razor light self. </summary>
+        /// <param name="services">     The services. </param>
+        /// <param name="environment">  The environment. </param>
+        /// <param name="root">         The root. </param>
+        private static void AddRazorLightSelf(this IServiceCollection services, IHostingEnvironment environment, string root)
+        {
+            services.AddSingleton<IRazorLightEngine>(f => new EngineFactory()
+                .ForFileSystem(System.IO.Path.Combine(environment.ContentRootPath, root)));
+        }
+    }
+}
