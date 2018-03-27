ee9b5c7fdf8565b48fdb6199820e5d33daae3d7f
Achim Schnell <a.schnell@wtschnell.de>
2018-03-27 15:14:01 +0200

    Moved ViewLocalization into ViewFiles

---
+++ src/FluiTec.AppFx.AspNetCore.Examples.AuthExample/MailModels/ErrorModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluiTec.AppFx.AspNetCore.Examples.AuthExample.Resources.MailViews;
+using FluiTec.AppFx.Mail;
+using FluiTec.AppFx.Localization;
+using Microsoft.Extensions.Localization;
+
+namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.MailModels
+{
+    /// <summary>   A data Model for the error. This class cannot be inherited. </summary>
+    public sealed class ErrorModel : MailModel
+    {
+        /// <summary>   Constructor. </summary>
+        /// <param name="localizerFactory"> The localizer factory. </param>
+        /// <param name="exception">        The exception. </param>
+        public ErrorModel(IStringLocalizerFactory localizerFactory, Exception exception)
+        {
+            var localizer = localizerFactory.Create(typeof(ErrorModelResource));
+
+            ApplicationName = MailGlobals.ApplicationName;
+            ApplicationUrl = MailGlobals.ApplicationUrl;
+            ApplicationUrlDisplay = MailGlobals.ApplicationUrlDisplay;
+
+            Subject = localizer.GetString(() => ErrorModelResource.Subject);
+            Header = localizer.GetString(() => ErrorModelResource.Header);
+            ExceptionPreText = localizer.GetString(() => ErrorModelResource.ExceptionPreText);
+
+            ExceptionText = exception?.ToString();
+        }
+
+        /// <summary>	Gets or sets the exception pre text. </summary>
+        /// <value>	The exception pre text. </value>
+        public string ExceptionPreText { get; set; }
+
+        /// <summary>	Gets or sets the exception text. </summary>
+        /// <value>	The exception text. </value>
+        public string ExceptionText { get; set; }
+
+        /// <summary>	Gets or sets the error route. </summary>
+        /// <value>	The error route. </value>
+        public string ErrorRoute { get; set; }
+    }
+}
---
+++ src/FluiTec.AppFx.AspNetCore.Examples.AuthExample/Resources/MailViews/ErrorModelResource.cs
@@ -0,0 +1,24 @@
+﻿using DbLocalizationProvider.Abstractions;
+
+namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Resources.MailViews
+{
+    /// <summary>   An error model resource. </summary>
+    [LocalizedResource]
+    public class ErrorModelResource
+    {
+        /// <summary>   Gets the subject. </summary>
+        /// <value> The subject. </value>
+        [TranslationForCulture("Auth-Beispiel Fehler", "de")]
+        public static string Subject => "Auth-Sample Error";
+
+        /// <summary>   Gets the header. </summary>
+        /// <value> The header. </value>
+        [TranslationForCulture("Unbekannter Fehler", "de")]
+        public static string Header => "Unknown error";
+
+        /// <summary>   Gets the exception pre text. </summary>
+        /// <value> The exception pre text. </value>
+        [TranslationForCulture("Es ist ein Fehler aufgetreten. Details:", "de")]
+        public static string ExceptionPreText => "An unexpected exception occured, details:";
+    }
+}
