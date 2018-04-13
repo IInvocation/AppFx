a7424b75c05e76742cb19a1295e7e1e409a619b4
Achim Schnell <a.schnell@wtschnell.de>
2018-04-13 13:44:17 +0200

    Added further views to manipulate localizations

--- src/FluiTec.AppFx.AspNetCore.Examples.AuthExample/Controllers/LocalizationController.cs
+++ src/FluiTec.AppFx.AspNetCore.Examples.AuthExample/Controllers/LocalizationController.cs
@@ -15,7 +15,7 @@ using Microsoft.Extensions.Localization;
 namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
 {
     /// <summary>   A controller for handling localizations. </summary>
-    [Authorize(PolicyNames.LocalizationAccess)]
+    [Authorize(PolicyNames.AdministrativeAccess)]
     public class LocalizationController : Controller
     {
         #region Fields
@@ -65,6 +65,7 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
         /// <param name="groupKey">     (Optional) The group key. </param>
         /// <param name="pageNumber">   (Optional) The page number. </param>
         /// <returns>   An IActionResult. </returns>
+        [Authorize(PolicyNames.LocalizationAccess)]
         public IActionResult Group(string groupKey = "", int pageNumber = 1)
         {
             if (groupKey == _localizer.GetString(r => r.Ungrouped)) groupKey = string.Empty;
@@ -98,10 +99,138 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
             }
         }
 
+        /// <summary>   Adds resource. </summary>
+        /// <returns>   An IActionResult. </returns>
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationCreate)]
+        public IActionResult AddResource()
+        {
+            var model = new ResourceAddModel
+            {
+                Author = User.GetDisplayName()
+            };
+            return View(model);
+        }
+
+        /// <summary>   (An Action that handles HTTP POST requests) adds a resource. </summary>
+        /// <param name="model">    The model. </param>
+        /// <returns>   An IActionResult. </returns>
+        [HttpPost]
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationCreate)]
+        public IActionResult AddResource(ResourceAddModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var uow = _localizationDataService.StartUnitOfWork())
+                {
+                    var resource = new ResourceEntity
+                    {
+                        ResourceKey = model.Name,
+                        Author = model.Author,
+                        FromCode = false,
+                        IsHidden = false,
+                        IsModified = false,
+                        ModificationDate = DateTime.Now
+                    };
+                    uow.ResourceRepository.Add(resource);
+                    uow.Commit();
+                    return RedirectToAction(nameof(EditResource), new { resourceId = resource.Id });
+                }          
+            }
+
+            return View();
+        }
+
+        /// <summary>   Edit resource. </summary>
+        /// <param name="resourceId">   Identifier for the resource. </param>
+        /// <returns>   An IActionResult. </returns>
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationUpdate)]
+        public IActionResult EditResource(int resourceId)
+        {
+            using (var uow = _localizationDataService.StartUnitOfWork())
+            {
+                var resource = uow.ResourceRepository.Get(resourceId);
+                return View(new ResourceEditModel
+                {
+                    Id = resourceId,
+                    Author = resource.Author,
+                    Name = resource.ResourceKey
+                });
+            }
+        }
+
+        /// <summary>   (An Action that handles HTTP POST requests) edit resource. </summary>
+        /// <param name="model">    The model. </param>
+        /// <returns>   An IActionResult. </returns>
+        [HttpPost]
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationUpdate)]
+        public IActionResult EditResource(ResourceEditModel model)
+        {
+            model.Update();
+            if (ModelState.IsValid)
+            {
+                using (var uow = _localizationDataService.StartUnitOfWork())
+                {
+                    var existing = uow.ResourceRepository.Get(model.Id);
+                    existing.ResourceKey = model.Name;
+                    existing.Author = model.Author;
+                    uow.ResourceRepository.Update(existing);
+                    uow.Commit();
+                    model.UpdateSuccess();
+                }
+            }
+
+            return View(model);
+        }
+
+        /// <summary>   Deletes the resource described by resourceId. </summary>
+        /// <param name="resourceId">   Identifier for the resource. </param>
+        /// <returns>   An IActionResult. </returns>
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationDelete)]
+        public IActionResult DeleteResource(int resourceId)
+        {
+            return View(new ResourceDeleteModel {Id = resourceId});
+        }
+
+        /// <summary>
+        ///     (An Action that handles HTTP POST requests) deletes the resource described by model.
+        /// </summary>
+        /// <param name="model">    The model. </param>
+        /// <returns>   An IActionResult. </returns>
+        [HttpPost]
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationDelete)]
+        public IActionResult DeleteResource(ResourceDeleteModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var uow = _localizationDataService.StartUnitOfWork())
+                {
+                    var existing = uow.ResourceRepository.Get(model.Id);
+                    if (existing != null)
+                    {
+                        foreach(var translation in uow.TranslationRepository.ByResource(existing))
+                            uow.TranslationRepository.Delete(translation);
+
+                        uow.ResourceRepository.Delete(existing);
+                        uow.Commit();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+            return View("Error");
+        }
+
         /// <summary>   Translations. </summary>
         /// <param name="resourceKey">  The resource key. </param>
         /// <param name="pageNumber">   (Optional) The page number. </param>
         /// <returns>   An IActionResult. </returns>
+        [Authorize(PolicyNames.LocalizationAccess)]
         public IActionResult Translations(string resourceKey, int pageNumber = 1)
         {
             using (var uow = _localizationDataService.StartUnitOfWork())
@@ -118,7 +247,9 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
         /// <summary>   Translations. </summary>
         /// <param name="id">   The identifier. </param>
         /// <returns>   An IActionResult. </returns>
-        public IActionResult Translation(int id)
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationUpdate)]
+        public IActionResult EditTranslation(int id)
         {
             using (var uow = _localizationDataService.StartUnitOfWork())
             {
@@ -135,7 +266,9 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
         /// <param name="model">    The model. </param>
         /// <returns>   An IActionResult. </returns>
         [HttpPost]
-        public IActionResult Translation(TranslationEditModel model)
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationUpdate)]
+        public IActionResult EditTranslation(TranslationEditModel model)
         {
             model.Update();
             if (ModelState.IsValid)
@@ -163,13 +296,15 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
         }
 
         /// <summary>   Adds a translation. </summary>
-        /// <param name="id">   The identifier. </param>
+        /// <param name="resourceKey">  The resource key. </param>
         /// <returns>   An IActionResult. </returns>
-        public IActionResult AddTranslation(int id)
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationCreate)]
+        public IActionResult AddTranslation(string resourceKey)
         {
             using (var uow = _localizationDataService.StartUnitOfWork())
             {
-                var resource = uow.ResourceRepository.Get(id);
+                var resource = uow.ResourceRepository.GetByKey(resourceKey);
                 if (resource == null) return RedirectToAction(nameof(Index));
                 return View(new TranslationAddModel { ResourceId = resource.Id });
             }
@@ -178,6 +313,9 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
         /// <summary>   Adds a translation. </summary>
         /// <param name="model">    The model. </param>
         /// <returns>   An IActionResult. </returns>
+        [HttpPost]
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationCreate)]
         public IActionResult AddTranslation(TranslationAddModel model)
         {
             if (ModelState.IsValid)
@@ -202,6 +340,44 @@ namespace FluiTec.AppFx.AspNetCore.Examples.AuthExample.Controllers
             }
 
             return View(model);
+        }
+
+        /// <summary>   Deletes the translation described by translationId. </summary>
+        /// <param name="translationId">    Identifier for the translation. </param>
+        /// <returns>   An IActionResult. </returns>
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationDelete)]
+        public IActionResult DeleteTranslation(int translationId)
+        {
+            return View(new TranslationDeleteModel {Id = translationId});
+        }
+
+        /// <summary>
+        ///     (An Action that handles HTTP POST requests) (Restricted to
+        ///     PolicyNames.LocalizationDelete) deletes the translation described by model.
+        /// </summary>
+        /// <param name="model">    The model. </param>
+        /// <returns>   An IActionResult. </returns>
+        [HttpPost]
+        [Authorize(PolicyNames.LocalizationAccess)]
+        [Authorize(PolicyNames.LocalizationDelete)]
+        public IActionResult DeleteTranslation(TranslationDeleteModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var uow = _localizationDataService.StartUnitOfWork())
+                {
+                    var existing = uow.TranslationRepository.Get(model.Id);
+                    if (existing != null)
+                    {
+                        uow.TranslationRepository.Delete(existing);
+                        uow.Commit();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+            return View("Error");
         }
     }
 }
