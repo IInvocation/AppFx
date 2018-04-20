9992400e96bbd90546e3512d2ddc4b6bfb48226b
Achim Schnell <a.schnell@wtschnell.de>
2018-04-20 12:47:10 +0200

    Added LanguageAware StatusCodeHandler-Method

--- src/FluiTec.AppFx.AspNetCore/StatusCodeExtension.cs
+++ src/FluiTec.AppFx.AspNetCore/StatusCodeExtension.cs
@@ -5,6 +5,7 @@ using FluiTec.AppFx.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 
 // ReSharper disable once CheckNamespace
@@ -70,6 +71,61 @@ namespace Microsoft.Extensions.DependencyInjection
                     var newPath = new PathString(
                         string.Format(CultureInfo.InvariantCulture, pathFormat,
                             context.HttpContext.Response.StatusCode));
+
+                    var originalPath = context.HttpContext.Request.Path;
+                    var originalQueryString = context.HttpContext.Request.QueryString;
+
+                    // Store the original paths so the app can check it.
+                    context.HttpContext.Features.Set<IStatusCodeReExecuteFeature>(new StatusCodeReExecuteFeature
+                    {
+                        OriginalPathBase = context.HttpContext.Request.PathBase.Value,
+                        OriginalPath = originalPath.Value,
+                        OriginalQueryString = originalQueryString.HasValue ? originalQueryString.Value : null
+                    });
+
+                    context.HttpContext.Request.Path = newPath;
+                    context.HttpContext.Request.QueryString = QueryString.Empty;
+                    try
+                    {
+                        await context.Next(context.HttpContext);
+                    }
+                    finally
+                    {
+                        context.HttpContext.Request.QueryString = originalQueryString;
+                        context.HttpContext.Request.Path = originalPath;
+                        context.HttpContext.Features.Set<IStatusCodeReExecuteFeature>(null);
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        ///     An IApplicationBuilder extension method that use status code handler with language.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are
+        ///                                             null. </exception>
+        /// <param name="app">              The app to act on. </param>
+        /// <param name="configuration">    The configuration. </param>
+        /// <returns>   An IApplicationBuilder. </returns>
+        public static IApplicationBuilder UseStatusCodeHandlerWithLanguage(this IApplicationBuilder app,
+           IConfigurationRoot configuration)
+        {
+            const string pathFormat = "/{0}/StatusCode/Index/{1}";
+
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            return app.UseStatusCodePages(async context =>
+            {
+                var requestLanguage = context.HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.Culture.TwoLetterISOLanguageName;
+                // check if we dont have an API-request here and want to handle the statuscode
+                if (!context.HttpContext.Request.Path.StartsWithSegments(_apiOptions.ApiOnlyPath)
+                    && _options.SelfHandledCodes.Contains(context.HttpContext.Response.StatusCode))
+
+                // if so - let the statuscodehandler do its stuff
+                {
+                    var newPath = new PathString(
+                        string.Format(CultureInfo.InvariantCulture, pathFormat,
+                            requestLanguage, context.HttpContext.Response.StatusCode));
 
                     var originalPath = context.HttpContext.Request.Path;
                     var originalQueryString = context.HttpContext.Request.QueryString;
