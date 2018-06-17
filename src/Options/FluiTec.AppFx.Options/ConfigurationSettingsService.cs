904cee265f2bd48b53762b14021376a072807077
Achim Schnell <a.schnell@wtschnell.de>
2018-06-17 12:30:08 +0200

    ...

--- src/Options/FluiTec.AppFx.Options/ConfigurationSettingsService.cs
+++ src/Options/FluiTec.AppFx.Options/ConfigurationSettingsService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace FluiTec.AppFx.Options
 {
@@ -13,6 +15,9 @@ namespace FluiTec.AppFx.Options
 
         /// <summary>	The configuration key. </summary>
         protected readonly string ConfigurationKey;
+
+        /// <summary>The logger.</summary>
+        private readonly ILogger _logger;
 
         /// <summary>	Options for controlling the operation. </summary>
         protected TSettings Settings;
@@ -28,6 +33,19 @@ namespace FluiTec.AppFx.Options
             Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
+        /// <summary>Constructor.</summary>
+        /// <param name="configuration">    The configuration. </param>
+        /// <param name="configKey">        The configuration key. </param>
+        /// <param name="loggerFactory">    The logger factory. </param>
+        public ConfigurationSettingsService(IConfiguration configuration, string configKey,
+            ILoggerFactory loggerFactory) : this(configuration, configKey)
+        {
+            if (loggerFactory != null)
+            {
+                _logger = loggerFactory.CreateLogger(GetType());
+            }
+        }
+
         /// <summary>	Gets the get. </summary>
         /// <returns>	The TSettings. </returns>
         public TSettings Get()
@@ -35,11 +53,15 @@ namespace FluiTec.AppFx.Options
             if (Settings != null)
                 return Settings;
 
+            _logger?.LogDebug($"Loading Configuration for \"{typeof(TSettings).Name}\" by Key \"{ConfigurationKey}\".");
+
             // load config-section
             var section = Configuration.GetSection(ConfigurationKey);
 
             // parse config-section
             Settings = section.Get<TSettings>();
+
+            _logger.LogDebug($"Parsed Configuration for \"{typeof(TSettings).Name}\", Content: {ObjectDumper.Dump(Settings)}");
 
             return Settings;
         }
